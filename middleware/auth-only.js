d1667c32ecfb69718fdb4b3d197ccfaa955bf3a8
Ryan Bump <me@ryanbump.com>
2017-04-05 16:12:24 -0500

    updated auth-only middleware to redirect to login page

--- middleware/auth-only.js
+++ middleware/auth-only.js
@@ -1,8 +1,9 @@
 var authOnly = function (req, res, next) {
     if (!req.auth) {
-        let error = new Error('Not authorized to access this uri.');
-        error.status = 401;
-        next(error);
+        // let error = new Error('Not authorized to access this uri.');
+        // error.status = 401;
+        // next(error);
+        res.redirect('/auth/login');
     }
 
     next()
